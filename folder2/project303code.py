bd89b69cbba85b89f65b75422a059497f4241273
Chimichongo <antonio.p.santacruz@gmail.com>
2023-10-13 12:48:24 -0500

    core
    core

--- folder2/project303code.py
+++ folder2/project303code.py
@@ -1,4 +1,20 @@
+#     _______             __                      _           __
+#    / ____(_)___  ____ _/ /  ____  _________    (_)__  _____/ /_
+#   / /_  / / __ \/ __ `/ /  / __ \/ ___/ __ \  / / _ \/ ___/ __/
+#  / __/ / / / / / /_/ / /  / /_/ / /  / /_/ / / /  __/ /__/ /_
+# /_/   /_/_/ /_/\__,_/_/  / .___/_/   \____/_/ /\___/\___/\__/
+#                         /_/              /___/
+#
+# By Antonio Santa Cruz
+#    Yuki Han
+#    Fanyimo
+#    Vincent Zhang
+####################################################################
+
 import ucimlrepo
+import numpy as np
+import pandaa as pd
+import matplotlib as plt
 from ucimlrepo import fetch_ucirepo
 
 # fetch dataset
@@ -12,4 +28,4 @@ y = support2.data.targets
 print(support2.metadata)
 
 # variable information
-print(support2.variables) 
+print(support2.variables)
